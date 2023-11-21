604650ed4a4a1972d7734c669e3541eaf4dfd6a8
Shkarupa Daniil <zshkarrr@outlook.com>
2023-11-21 22:01:29 +0200

    Update amount of cards

--- src/shared/utils/deck.ts
+++ src/shared/utils/deck.ts
@@ -11,7 +11,7 @@ export class Deck {
     }
 
     private initializeDeck(): void {
-        for(let i = 1; i <= 100; i++) {
+        for(let i = 1; i <= 115; i++) {
             this.cards.push(i.toString());
         }
         
